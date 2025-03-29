74bae0af4f294305313d5d8a40487ac947870784
Suzune Horikita <51443626+horikitas@users.noreply.github.com>
2025-03-30 00:05:36 +0530

    Fixed Tests - CB works, Reactive doesn't support annotations, onErrorResume works. Haven't tested time limiter and rate limiter.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-Trying Reslience4j Reactive
+## Trying Reslience4j Reactive
+
+### 1. CLOSED State
+1. Everything looks fine. Let requests flow.
+2. All calls are allowed through.
+3. Failures are recorded.
+4. If failure rate exceeds a threshold (e.g., 50%) over a sliding window, the circuit transitions to OPEN.
+
+### 2. OPEN State
+1. Too many failures. Stop! Don’t call the service.
+2. No calls are allowed through — they fail fast with a CallNotPermittedException.
+3. After a configurable waitDurationInOpenState, the breaker transitions to HALF_OPEN to test recovery.
+
+### HALF_OPEN State 
+1. Let me test a few requests. Maybe the service is back? 
+2. Allows only a limited number of trial calls. 
+3. If a certain number of successes occur → transitions back to CLOSED. 
+4. If any trial call fails → goes back to OPEN.
+
+**Visual Representation of Circuit Breaker States:**
+![cb_visual_summary.png](cb_visual_summary.png)
+
+**These are special, usually manual states:**
+1. DISABLED: No protection, breaker does nothing.
+2. FORCED_OPEN: Always blocks calls, like OPEN but never transitions.
+3. FORCED_CLOSED: Always allows calls, like CLOSED but never transitions.
--- src/main/java/org/horikita/controller/DemoController.java
+++ src/main/java/org/horikita/controller/DemoController.java
@@ -1,12 +1,17 @@
 package org.horikita.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -17,38 +22,64 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 class DemoController {
 
-    private final AtomicInteger counter = new AtomicInteger(0);
-
+    @Autowired
+    private CounterProvider counterProvider;
     public static final int FAILURE_SIMULATOR_COUNT = 3;
 
-    @GetMapping("/unstable")
-    @CircuitBreaker(name = "unstableService", fallbackMethod = "fallback")
-    @Retry(name = "unstableService", fallbackMethod = "retryFallback")
-    public Mono<ResponseEntity<String>> unstableEndpoint() {
+    @Autowired
+    private CircuitBreaker cb;
 
-        return Mono.fromSupplier(counter::incrementAndGet)
-                .doOnNext(current -> log.info("Attempt: {}", current))
+    /*
+    .transform(TimeLimiterOperator.of(timeLimiter))
+    .transform(CircuitBreakerOperator.of(circuitBreaker))
+    .transform(RetryOperator.of(retry))
+     */
+    @GetMapping("/unstable")
+    public Mono<ResponseEntity<String>> unstableEndpoint(@RequestParam(value = "simulateFail", defaultValue = "false") boolean simulateFail) {
+        log.info("Circuit Breaker State when call is made: {}", cb.getState());
+        log.info("Failed count {}", cb.getMetrics().getNumberOfFailedCalls());
+
+        return callExternalAPI(simulateFail)
+                .transform(CircuitBreakerOperator.of(cb))
+                .onErrorResume(e -> {
+                    log.error("Error occurred: {}", e.getMessage());
+                    if (e instanceof CallNotPermittedException) {
+                       return closedFallback(Mono.just(e)); // Handle circuit breaker open state
+                    } else {
+                        return retryFallback(Mono.just(e)); // Handle other errors
+                    }
+                });
+    }
+
+    private Mono<ResponseEntity<String>> retryFallback(Mono<Throwable> error) {
+        return error.flatMap(e -> {
+            if (e instanceof CallNotPermittedException) {
+                log.error("Circuit Breaker is open: {}, cbState {}", e.getMessage(), cb.getState());
+                return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Final Fallback: Circuit Breaker is open"));
+            }
+            log.warn("Retry Fallback triggered: {}, cbState {}", e.getMessage(), cb.getState());
+            return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Retry Fallback: " + e.getMessage()));
+        });
+    }
+
+
+    private Mono<ResponseEntity<String>> closedFallback(Mono<Throwable> error) {
+        return error.flatMap(e -> {
+            log.warn("Final Fallback triggered: {}", e.getMessage());
+            return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Fallback: " + e.getMessage()));
+        });
+    }
+
+    private Mono<ResponseEntity<String>> callExternalAPI(boolean simulateFail) {
+        return Mono.fromSupplier(() -> counterProvider.getCounter().incrementAndGet())
                 .flatMap(current -> {
-                    if (current % FAILURE_SIMULATOR_COUNT == 0) {
-                        log.warn("Simulating failure for attempt {}", current);
+                    if (simulateFail) {
+                        log.warn("Simulating failure for counter {}", current);
                         return Mono.error(new RuntimeException("Simulated failure on attempt " + current));
                     }
                     return Mono.just(ResponseEntity.ok("Success " + current));
                 });
     }
 
-    public Mono<ResponseEntity<String>> retryFallback(Mono<Throwable> error) {
-        return error.flatMap(e -> {
-            log.warn("Retry Fallback triggered: {}", e.getMessage());
-            return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Fallback: " + e.getMessage()));
-        });
-    }
 
-
-    public Mono<ResponseEntity<String>> fallback(Mono<Throwable> error) {
-        return error.flatMap(e -> {
-            log.warn("Fallback triggered: {}", e.getMessage());
-            return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Fallback: " + e.getMessage()));
-        });
-    }
 }
