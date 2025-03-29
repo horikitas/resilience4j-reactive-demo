2ad8c08844b5aafa1b736f937194cae23f1a8e4b
Suzune Horikita <51443626+horikitas@users.noreply.github.com>
2025-03-30 00:05:50 +0530

    Fixed Tests - CB works, Reactive doesn't support annotations, onErrorResume works. Haven't tested time limiter and rate limiter.

---
+++ src/main/java/org/horikita/controller/CBconfig.java
@@ -0,0 +1,39 @@
+package org.horikita.controller;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Slf4j
+@Configuration
+public class CBconfig {
+
+    @Bean
+    public CircuitBreaker circuitBreaker() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(30) // percentage
+                .minimumNumberOfCalls(10)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .waitDurationInOpenState(Duration.ofMillis(500))
+                .slidingWindowSize(10)
+                .build();
+        CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.of(config);
+        CircuitBreaker cb = circuitBreakerRegistry.circuitBreaker("unstableEndpoint");
+
+
+        logCircuitBreaker(cb);
+        return cb;
+    }
+
+    private void logCircuitBreaker(CircuitBreaker cb) {
+        log.info("Request received for unstable endpoint");
+        log.info("Circuit Breaker getFailureRateThreshold: {}", cb.getCircuitBreakerConfig().getFailureRateThreshold());
+        log.info("Circuit Breaker getMinimumNumberOfCalls: {}", cb.getCircuitBreakerConfig().getMinimumNumberOfCalls());
+        log.info("Circuit Breakder Name: {}", cb.getName());
+    }
+}
